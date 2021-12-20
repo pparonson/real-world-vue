fe8f9b4d5566be9722513b318bf9823e89c61a83
paronson <paronson@qw-corp.com>
2021-12-20 15:56:04 -0600

    completed L6-start

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+  name: 'EventDetails',
+  components: {},
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+      /* id: 123 */
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style scoped>
+.event {
+  align-items: center;
+}
+</style>
